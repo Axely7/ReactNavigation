a44da2ec51f26f859c7cdef458748a0a9e9262d5
Axel Jimenez <axely7@hotmail.com>
2021-09-14 15:12:37 -0500

    Sendind parameters through pages

---
+++ App.js
@@ -0,0 +1,42 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+//React Navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+
+import Inicio from './views/Inicio';
+import Nosotros from './views/Nosotros';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Inicio"
+        >
+          <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+            options={{
+              title: "Componente Principal"
+            }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Nosotros"
+            component={Nosotros}
+            options={ ({route}) => ({
+              title: route.params.clienteId
+            })}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>    
+    </>
+  );
+};
+
+export default App;
